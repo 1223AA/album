af589ab77e589e8fcab5d374c9df4cbc55180c9f
zhang yuan ze <101808816+1223AA@users.noreply.github.com>
2022-06-24 02:47:03 +0800

    Add files via upload

---
+++ indexl.js
@@ -0,0 +1,237 @@
+<!doctype html>
+<!--[if lt IE 7 ]> <html lang="en" class="ie6"> <![endif]-->
+<!--[if IE 7 ]>    <html lang="en" class="ie7"> <![endif]-->
+<!--[if IE 8 ]>    <html lang="en" class="ie8"> <![endif]-->
+<!--[if IE 9 ]>    <html lang="en" class="ie9"> <![endif]-->
+<!--[if !IE]><!--> <html lang="en"> <!--<![endif]-->
+<head>
+<meta name="viewport" content="width = 1050, user-scalable = no" />
+<script type="text/javascript" src="extras/jquery.min.1.7.js"></script>
+<script type="text/javascript" src="extras/modernizr.2.5.3.min.js"></script>
+
+</head>
+<body>
+
+<div class="flipbook-viewport">
+	<div class="container">
+		<div class="flipbook">
+
+			<div style="text-align: center;">
+				<br>
+				<font style="font-size: 20px;">作品集</font><br><br>
+				<font style="font-size: 12px;">課程名稱：數位敘事實作與應用</font><br>
+				<font style="font-size: 12px;">學號：410717946</font><br>
+				<font style="font-size: 12px;">系級：大傳四A</font><br>
+				<font style="font-size: 12px;">姓名：張元澤</font><br>
+				<font style="font-size: 12px;">指導老師：王錦裕老師</font><br>
+				<br>
+				<br>
+				<img src="pages/0.jpg" style="max-width: 95%; height: auto;">
+			</div>
+
+			<div style="text-align: left;">
+				<ol start="0">
+
+					<li style="font-size: 20px;list-style-type: none;">
+						目  錄<br><br>
+					</li>
+
+					<li style="font-size: 14px;">台中市景點網頁設計<br>
+						<a href="file:///C:/Users/User/Desktop/%E5%BC%B5%E5%85%83%E6%BE%A4%20410717946%20%E4%BD%9C%E4%B8%9A1.html" target="_blank">file:///C:/Users/User/Desktop/%E5%BC%B5%E5%85%83%E6%BE%A4%20410717946%20%E4%BD%9C%E4%B8%9A1.html</a>
+					</li>
+
+					<li style="font-size: 14px;">個人資訊網<br>
+						<a href="file:///C:/Users/User/Desktop/%E4%B8%AA%E4%BA%BA%E4%BB%8B%E7%BB%8D.html" target="_blank">file:///C:/Users/User/Desktop/%E4%B8%AA%E4%BA%BA%E4%BB%8B%E7%BB%8D.html</a>
+					</li>
+
+					<li style="font-size: 14px;">地圖敘事(使用GoogleMyMap)<br>
+						<a href="https://www.google.com/maps/d/u/0/viewer?mid=16F0sM4CY13_KBcNPWY3nv36cHuOqLIy6&ll=24.13820582331916%2C120.75263000000002&z=9" target="_blank">https://www.google.com/maps/d/u/0/viewer?mid=16F0sM4CY13_KBcNPWY3nv36cHuOqLIy6&ll=24.13820582331916%2C120.75263000000002&z=9</a>
+					</li>
+
+					<li style="font-size: 14px;">圖表敘事-SVG實作(使用RawGraph)<br>
+						<a href="https://xx/" target="_blank">https://xx/</a>
+					</li>
+
+					<li style="font-size: 14px;">圖表敘事-LineChart實作(使用HighCharts)<br>
+						<a href="https://1223aa.github.io/tourist/" target="_blank">https://1223aa.github.io/tourist/</a>
+					</li>
+
+					<li style="font-size: 14px;">圖表敘事-PieChart實作(使用HighCharts)<br>
+						<a href="https://github.com/1223AA/student" target="_blank">https://github.com/1223AA/student</a>
+					</li>
+
+					<li style="font-size: 14px;">故事地圖實作(使用KnightLab)<br>
+						<a href="https://cdn.knightlab.com/libs/storyline/latest/embed/index.html?dataURL=https%3A%2F%2Fdocs.google.com%2Fspreadsheets%2Fd%2F1Io856DYJmMrZrYgVAfxPdHRnTBrLlmGjJUKWbttp7Zs%2Fpubhtml&dataYCol=temperaturechange&dataXCol=year&dataDateFormat=%25Y&chartDateFormat=%25Y&chartYLabel=Annual%20Temp.%20Change&sliderCardTitleCol=title&sliderCardTextCol=text" target="_blank">https://cdn.knightlab.com/libs/storyline/latest/embed/index.html?dataURL=https%3A%2F%2Fdocs.google.com%2Fspreadsheets%2Fd%2F1Io856DYJmMrZrYgVAfxPdHRnTBrLlmGjJUKWbttp7Zs%2Fpubhtml&dataYCol=temperaturechange&dataXCol=year&dataDateFormat=%25Y&chartDateFormat=%25Y&chartYLabel=Annual%20Temp.%20Change&sliderCardTitleCol=title&sliderCardTextCol=text</a>
+					</li>
+
+					<li style="font-size: 14px;">虛擬實境實作(使用Krpano)<br>
+						<a href="C:\Users\User\Desktop\vtour\index.html.xml" target="_blank">C:\Users\User\Desktop\vtour\index.html.xml</a>
+					</li>
+
+					<li style="font-size: 14px;">互動式魚骨圖實作(使用amCharts)<br>
+						<a href="file:///C:/Users/User/Desktop/%E9%B1%BC%E9%AA%A8%E5%9B%BE.html" target="_blank">file:///C:/Users/User/Desktop/%E9%B1%BC%E9%AA%A8%E5%9B%BE.html</a>
+					</li>
+
+					<li style="font-size: 14px;">互動簡報製作(使用Prezi)<br>
+						<a href="https://prezi.com/v/zsssupbg0ivr/untitled/" target="_blank">https://prezi.com/v/zsssupbg0ivr/untitled/</a>
+					</li>
+
+					<li style="font-size: 14px;">動畫影片製作(使用Powtoon)<br>
+						<a href="https://www.powtoon.com/html5-studio/?#/edit/bZwA4688IK3" target="_blank">https://www.powtoon.com/html5-studio/?#/edit/bZwA4688IK3</a>
+					</li>
+
+					<li style="font-size: 14px;">地理資訊系統實作(使用Leaflet)<br>
+						<a href="https://xx/" target="_blank">https://xx/</a>
+					</li>
+				</ol>
+			</div>
+
+			<div style="text-align: center;">
+				<br>
+				<font style="font-size: 18px;">台中市景點網頁設計</font>
+				<a href="file:///C:/Users/User/Desktop/%E5%BC%B5%E5%85%83%E6%BE%A4%20410717946%20%E4%BD%9C%E4%B8%9A1.html" target="_blank">進入作品</a>
+				<br><br>
+				<iframe src="file:///C:/Users/User/Desktop/%E5%BC%B5%E5%85%83%E6%BE%A4%20410717946%20%E4%BD%9C%E4%B8%9A1.html" frameborder="0" scrolling="no" width="400" height="490"></iframe>
+			</div>
+
+			<div style="text-align: center;">
+				<br>
+				<font style="font-size: 18px;">個人資訊網</font>
+				<a href="file:///C:/Users/User/Desktop/%E4%B8%AA%E4%BA%BA%E4%BB%8B%E7%BB%8D.html" target="_blank">進入作品</a>
+				<br><br>
+				<iframe src="file:///C:/Users/User/Desktop/%E4%B8%AA%E4%BA%BA%E4%BB%8B%E7%BB%8D.html" frameborder="0" scrolling="no" width="400" height="490"></iframe>
+			</div>
+
+			<div style="text-align: center;">
+				<br>
+				<font style="font-size: 18px;">地圖敘事(使用GoogleMyMap)</font>
+				<a href="https://www.google.com/maps/d/u/0/viewer?mid=16F0sM4CY13_KBcNPWY3nv36cHuOqLIy6&ll=24.13820582331916%2C120.75263000000002&z=9" target="_blank">進入作品</a>
+				<br><br>
+				<iframe src="https://www.google.com/maps/d/u/0/viewer?mid=16F0sM4CY13_KBcNPWY3nv36cHuOqLIy6&ll=24.13820582331916%2C120.75263000000002&z=9" frameborder="0" scrolling="no" width="400" height="490"></iframe>
+			</div>
+
+			<div style="text-align: center;">
+				<br>
+				<font style="font-size: 18px;">圖表敘事-SVG實作(使用RawGraph)</font>
+				<a href="https://xx/" target="_blank">進入作品</a>
+				<br><br>
+				<iframe src="https://xx/" frameborder="0" scrolling="no" width="400" height="490"></iframe>
+			</div>
+
+			<div style="text-align: center;">
+				<br>
+				<font style="font-size: 18px;">圖表敘事-LineChart實作(使用HighCharts)</font>
+				<a href="https://1223aa.github.io/tourist/" target="_blank">進入作品</a>
+				<br><br>
+				<iframe src="https://1223aa.github.io/tourist/" frameborder="0" scrolling="no" width="400" height="490"></iframe>
+			</div>
+
+			<div style="text-align: center;">
+				<br>
+				<font style="font-size: 18px;">圖表敘事-PieChart實作(使用HighCharts)</font>
+				<a href="https://github.com/1223AA/student" target="_blank">進入作品</a>
+				<br><br>
+				<iframe src="https://github.com/1223AA/student" frameborder="0" scrolling="no" width="400" height="490"></iframe>
+			</div>
+
+			<div style="text-align: center;">
+				<br>
+				<font style="font-size: 18px;">故事地圖實作(使用KnightLab)</font>
+				<a href="https://cdn.knightlab.com/libs/storyline/latest/embed/index.html?dataURL=https%3A%2F%2Fdocs.google.com%2Fspreadsheets%2Fd%2F1Io856DYJmMrZrYgVAfxPdHRnTBrLlmGjJUKWbttp7Zs%2Fpubhtml&dataYCol=temperaturechange&dataXCol=year&dataDateFormat=%25Y&chartDateFormat=%25Y&chartYLabel=Annual%20Temp.%20Change&sliderCardTitleCol=title&sliderCardTextCol=text" target="_blank">進入作品</a>
+				<br><br>
+				<iframe src="https://cdn.knightlab.com/libs/storyline/latest/embed/index.html?dataURL=https%3A%2F%2Fdocs.google.com%2Fspreadsheets%2Fd%2F1Io856DYJmMrZrYgVAfxPdHRnTBrLlmGjJUKWbttp7Zs%2Fpubhtml&dataYCol=temperaturechange&dataXCol=year&dataDateFormat=%25Y&chartDateFormat=%25Y&chartYLabel=Annual%20Temp.%20Change&sliderCardTitleCol=title&sliderCardTextCol=text" frameborder="0" scrolling="no" width="400" height="490"></iframe>
+			</div>
+
+			<div style="text-align: center;">
+				<br>
+				<font style="font-size: 18px;">虛擬實境實作(使用Krpano)</font>
+				<a href="C:\Users\User\Desktop\vtour\index.html.xml" target="_blank">進入作品</a>
+				<br><br>
+				<iframe src="C:\Users\User\Desktop\vtour\index.html.xml" frameborder="0" scrolling="no" width="400" height="490"></iframe>
+			</div>
+
+			<div style="text-align: center;">
+				<br>
+				<font style="font-size: 18px;">互動式魚骨圖實作(使用amCharts)</font>
+				<a href="file:///C:/Users/User/Desktop/%E9%B1%BC%E9%AA%A8%E5%9B%BE.html" target="_blank">進入作品</a>
+				<br><br>
+				<iframe src="file:///C:/Users/User/Desktop/%E9%B1%BC%E9%AA%A8%E5%9B%BE.html" frameborder="0" scrolling="no" width="400" height="490"></iframe>
+			</div>
+
+			<div style="text-align: center;">
+				<br>
+				<font style="font-size: 18px;">互動簡報製作(使用Prezi)</font>
+				<a href="https://prezi.com/v/zsssupbg0ivr/untitled/" target="_blank">進入作品</a>
+				<br><br>
+				<img src="prize1.png" style="max-width: 95%; height: auto;">
+				<br>
+				<img src="prize2.png" style="max-width: 95%; height: auto;">
+			</div>
+
+			<div style="text-align: center;">
+				<br>
+				<font style="font-size: 18px;">動畫影片製作(使用Powtoon)</font>
+				<a href="https://www.powtoon.com/html5-studio/?#/edit/bZwA4688IK3" target="_blank">進入作品</a>
+				<br><br>
+				<img src="powtoon1.png" style="max-width: 95%; height: auto;">
+				<br>
+				<img src="powtoon2.png" style="max-width: 95%; height: auto;">
+			</div>
+
+			<div style="text-align: center;">
+				<br>
+				<font style="font-size: 18px;">地理資訊系統實作(使用Leaflet)</font>
+				<a href="https://xx/" target="_blank">進入作品</a>
+				<br><br>
+				<iframe src="https://xx/" frameborder="0" scrolling="no" width="400" height="490"></iframe>
+			</div>
+
+		</div>
+	</div>
+</div>
+
+
+<script type="text/javascript">
+
+function loadApp() {
+
+	// Create the flipbook
+
+	$('.flipbook').turn({
+			// Width
+
+			width:922,
+			
+			// Height
+
+			height:600,
+
+			// Elevation
+
+			elevation: 50,
+			
+			// Enable gradients
+
+			gradients: true,
+			
+			// Auto center this flipbook
+
+			autoCenter: true
+
+	});
+}
+
+// Load the HTML4 version if there's not CSS transform
+
+yepnope({
+	test : Modernizr.csstransforms,
+	yep: ['lib/turn.js'],
+	nope: ['lib/turn.html4.min.js'],
+	both: ['css/basic.css'],
+	complete: loadApp
+});
+
+</script>
+
+</body>
+</html>
